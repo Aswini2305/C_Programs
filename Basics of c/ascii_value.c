018ce4429b6776f2471334958b8ecc8cdf5c0ab0
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2021-06-15 13:03:42 +0530

    ascii value of character

---
+++ Basics of c/ascii_value.c
@@ -0,0 +1,9 @@
+//getting ascii value of character
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("enter any character\n");
+    scanf("%c",&c);
+    printf("The ASCII value of %c = %d",c,c);
+}
