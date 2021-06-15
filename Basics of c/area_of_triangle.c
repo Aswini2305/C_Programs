37723916d6425fca1c6ab73a6356863e7197f7b2
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2021-06-15 13:35:44 +0530

    Area of triangle

---
+++ Basics of c/area_of_triangle.c
@@ -0,0 +1,9 @@
+//Area of triangle
+#include<stdio.h>
+int main()
+{
+    float base,height;
+    printf("Enter the base and height of the triangle\n");
+    scanf("%f%f",&base,&height);
+    printf("Area of triangle%f",(height*base)/2);
+}
