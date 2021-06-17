4616a932725c654f89554777ea731fb392b050da
Aswini2305 <72400882+Aswini2305@users.noreply.github.com>
2021-06-17 12:10:54 +0530

    factorial of a number

---
+++ Basics of c/fact.c
@@ -0,0 +1,20 @@
+//Factorial of a number using while loop
+#include<stdio.h>
+int
+main ()
+{
+  int fact = 1, n, i=1;
+  scanf ("%d", &n);
+  if (n <= 0)
+    printf ("Oops! we can't find factorial for negative numbers");
+  else
+    {
+      while (i <= n)
+	{
+	  fact *= i;
+	  i++;
+	}
+    printf ("factorial of %d is %d", n, fact);
+        
+    }
+}
